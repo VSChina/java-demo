790414503cf3aa146dc3bff133a874c031034f22
Jun Han <junhan@microsoft.com>
2017-10-25 14:42:45 +0800

    Improvement

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,8 +3,15 @@ $.ajax({
         url: "https://fabrikam-functions-java-demo.azurewebsites.net/api/register?ua=" + window.navigator.userAgent,
     })
     .done(function () {
-        $("#result").text("签到成功！");
+        $("#result-text").text("签到成功");
+        $("#result-img").attr("src", "images/sign_success@3x.png");
     })
     .fail(function (jqXHR, textStatus, errorThrown) {
-        $("#result").text("签到失败！");
-    });
+        $("#result-text").text("签到失败");
+        $("#result-img").attr("src", "images/sign_fail@3x.png");
+        $("#result-text").css("color", "#f55320");
+        $("#try").show();
+    })
+    .always(function () {
+        $("#result-img").css("height", "5%");
+    })
